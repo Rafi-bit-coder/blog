0b7ed2583f88d11effa3fa0112c20219f1b6fe68
rafi <muhrafidwi2@gmail.com>
2025-03-19 21:14:31 +0800

    using websocket as the bridge

--- BACKEND/main.js
+++ BACKEND/main.js
@@ -1,5 +1,20 @@
+const db = require("../API/db.json")
+const fs = require('fs')
 const ws = require('ws')
 const http = require('http')
+let previusData = null
+
+const readJsonFile = () => {
+    try{
+        const data = fs.readFileSync('../API/db.json')
+        return JSON.parse(data, "utf8")
+    }
+    catch (err){
+        console.error(`error occured: `, err)
+        return null
+    }
+}
+
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {    "Content-Type": "text/plain"    })
@@ -10,11 +25,29 @@ const wss = new ws.Server({  server })
 
 wss.on('connection', (ws) => {
     console.log('client connected')
+    
+    // console.log(JSON.stringify(db))
+    
+    fetch("http://127.0.0.1:3000/posts")
+    .then(res => res.json())
+    .then(data => {
+        ws.send(JSON.stringify(data))
+    })
+    fs.watchFile("../API/db.json", {interval: 1000}, (curr, prev) => {
+        const newData = readJsonFile()
+        if(JSON.stringify(newData) !== JSON.stringify(previusData)) {
+            console.log("JSON file updated:" ,newData)
+            previusData = newData
+            ws.send(newData)
+        }
+    })
 
     // setInterval(() => {ws.send('hello there')}, 2000)
 
     ws.on("message", (message) => {
         let data = message.toString("utf-8")
+
+        // if()
         console.log('received message: ', data)
     })
 
--- FRONTEND/index.js
+++ FRONTEND/index.js
@@ -1,16 +1,27 @@
 const ws = new WebSocket("ws://localhost:8080")
+let connected = false
 
 ws.onopen = () => {
+    connected = true
     console.log('connected to server')
     ws.send("hello server")
-
 }
 
 ws.onmessage = (event) => {
-    console.log('data dari server: ', event.data)
+    let data = JSON.parse(event.data)
+    data.map(post => {
+        container.innerHTML += `
+            <a href="/posts/?id=${post.id}&blog=${post.blog_name}" class="posts ${post.id}">
+                <h2>${post.blog_name}</h1>
+                <p>${post.point}</p>
+            </a>
+        `
+    })
+    console.log('data dari server: ', JSON.parse(data))
 }
 
 ws.onclose = () => {
+    connected = false
     console.log('disconnected from server')
 }
 
