788c0f6b1d47c20a50a276554c7ba59c2eae0bcb
rafi <muhrafidwi2@gmail.com>
2025-04-19 21:42:51 +0800

    now you dont have to press post button you just need to press enter to upload the comment

--- FRONTEND/posts/post.js
+++ FRONTEND/posts/post.js
@@ -15,26 +15,31 @@ const BLOG_CREATOR = document.querySelector('.blog-creator')
 // comment section
 const comment_container = document.querySelector('.comment-point')
 const comment_value = document.querySelector('.add-comment')
-const post_comment = document.querySelector('.post-comment')
+// const post_comment = document.querySelector('.post-comment')
 
-comment_value.addEventListener('input', (e) => {
-    autoResize(e.target)
-    e.target.value.length !== 0 ? post_comment.classList.add('show') : post_comment.classList.remove('show')
+comment_value.addEventListener('keydown', (e) => {
+    // autoResize(e.target)
+    // e.target.textContent.length !== 0 ? post_comment.classList.add('show') : post_comment.classList.remove('show')
+    if (e.target.textContent !== 0) {
+        if (e.key === "Enter" && !e.shiftKey) {
+            e.preventDefault();
+            postCommen()
+        }
+    }
 })
 
-function autoResize(textArea) {
-    textArea.style.height = 'auto'
-    textArea.style.height = textArea.scrollHeight + 'px'
-}
+// function autoResize(textArea) {
+//     textArea.style.height = '19px'
+//     textArea.style.height = textArea.scrollHeight + 'px'
+//     console.log(textArea.scrollHeight)
+// }
 
 function postCommen () {
-    let value = comment_value.value
-    fetch(`http://127.0.0.1:8080/comment`, {method: 'POST', body: JSON.stringify({blogId: id, comment: value,
+    let value = comment_value.textContent
+    fetch(`http://127.0.0.1:8080/comment`, {method: 'POST', body: JSON.stringify({blogId: id, comment: value.trim(),
         user: temporary
     })})
 }
-
-post_comment.onclick = postCommen
 
 fetch(`http://127.0.0.1:8080/posts?id=${id}`, {method: "GET"})
 .then(res => res.json())
@@ -46,7 +51,7 @@ fetch(`http://127.0.0.1:8080/posts?id=${id}`, {method: "GET"})
             <h3>${data.body.point}</h3>
         `
         BLOG_CREATOR.innerHTML = `
-            <h2>${data.userid.userName}</h2>
+            <a href="#"><h2>${data.userid.userName}</h2></a>
         `
         if(data.body.comments?.length) {
             data.body.comments?.map(a => {
